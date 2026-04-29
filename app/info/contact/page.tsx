c7d778be87a87b8396bd68cfaebde2388bbea259
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-29 07:53:09 +0000

    nestedRoutes: addd a nested routes ie the info/contact

---
+++ app/info/contact/page.tsx
@@ -0,0 +1,18 @@
+
+const Contact = () => {
+  return (
+    <div>
+          <h1 className="text-7xl">
+              this the contact page!!!
+          </h1>
+          <h2 className="text-xl font-bold text-center m-4  text-green-500">
+              Mandeeya
+          </h2>
+          <p className="m-1 text-xl inline-block">
+              Nested routing in next.js is awesome
+          </p>
+    </div>
+  )
+}
+
+export default Contact
