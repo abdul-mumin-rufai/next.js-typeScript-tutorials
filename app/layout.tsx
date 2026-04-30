b9abe3ad81f32295cb9d3bf28fd932a38d14914d
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-30 19:22:30 +0000

    font: added google font to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import Navbar from '@/components/navbar';
 import './globals.css';
+import { Inter, Inconsolata, Roboto } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+const inconsolata = Inconsolata({ subsets: ['latin'] });
+const roboto = Roboto({ subsets: ['latin'], weight: ['400'] }); // some font need weight property just like Roboto
+
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) { 
+  // Layout allow us to share UI within other pages.
   return (
     <html lang='en'>
-      <body>
+      <body className={roboto.className}>
         <Navbar />
         <main className='max-w-3xl mx-auto py-4 flex gap-x-4'>
           {children}
