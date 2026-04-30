23c6e8c1880be078d0e786f70a93cd2c642d6656
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-30 21:46:56 +0000

    metadata: provided a metadata for the tutorial project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
 import Navbar from '@/components/navbar';
 import './globals.css';
 import { Inter, Inconsolata, Roboto } from 'next/font/google';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 const inconsolata = Inconsolata({ subsets: ['latin'] });
 const roboto = Roboto({ subsets: ['latin'], weight: ['400'] }); // some font need weight property just like Roboto
 
+
+// metadata is provided in the html head to give us more infprmation
+export const metadata: Metadata = {
+  title: "Rufai next.js",
+  description: "A comprehensive next.js with typescript tutorial to gain the fundamentals of next.js",
+  keywords:"next.js, typescript and tailwind css"
+};
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) { 
   // Layout allow us to share UI within other pages.
