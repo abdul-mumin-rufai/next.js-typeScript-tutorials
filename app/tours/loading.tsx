87723e50a4ec9b926abedc3bc4eacdc6aa9f7b87
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-02 14:35:19 +0000

    loading component: added a loading component to display loading message whiles the data is being fetch

---
+++ app/tours/loading.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Loading = () => {
+  return (
+      <span className=" font-bold text-3xl capitalize">
+          please wait page is loading...
+    </span>
+  )
+}
+
+export default Loading;
