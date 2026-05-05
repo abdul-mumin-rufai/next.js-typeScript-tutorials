c59bda5d78227a7c216ae5ef1995e4b22559d689
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-05 19:59:38 +0000

    refactor: changed the background of the nested layout to green

--- app/tours/layout.tsx
+++ app/tours/layout.tsx
@@ -1,7 +1,7 @@
 const ToursLayout = ({children}:{children:React.ReactNode}) => {
   return (
       <div>
-          <header className='py-2 bg-slate-500 rounded mb-4'>
+          <header className='py-2 bg-green-500 rounded mb-4'>
               <h1 className='text-3xl text-white text-center capitalize'>
                 tours layout (nested layout)
               </h1>
