d031e8ee2901dd40b1b113bfe863a902f2b8e00f
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-06 09:34:56 +0000

    images: added a local image to the project. In the dynamic page i added Rufai.png image

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import rufaiImg from '@/images/Rufai.png';
+import Image from 'next/image';
 
 const ToursId = ({ params }: { params: { id: string } }) => {
-    console.log(params);
+  console.log(params);
   return (
-      <div className=' text-4xl'>
-          <h2>this is the ID: { params.id}</h2>
+    <div className=' text-4xl'>
+      <h2>this is the ID: {params.id}</h2>
+      <section className='flex gag-x-4 mt-4'>
+        <div>
+          {/*  local image */}
+          <Image src={rufaiImg}
+            alt='RufaiImage'
+            width={192}
+            height={192}
+            className='w-48 h-48 object-cover rounded'
+            priority
+          />
+          <h2 className='text-3xl font-bold'>
+            Local Image
+          </h2>
+        </div>
+      </section>
     </div>
   )
-}
+};
 
 export default ToursId;
