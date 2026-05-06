67bcfb0fd4050fd22881c0b8e147b6d7fb5e2a07
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-06 16:33:13 +0000

    images: added responsive images to the project

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const url = 'https://www.course-api.com/react-tours-project';
 
 interface Toursdata { 
@@ -26,16 +26,29 @@ const Tours = async () => {
                 this is the tours page
             </h1>
             <section>
-                {tours.map((tour) => {
-                    return (
-                        <Link className="hover:text-blue-500" key={tour.id} href={`/tours/${tour.id}`}>
-                            <h2 >
-                                {tour.name}
-                            </h2>
-                        </Link>
-                        
-                    )
-                })}
+                <div className="grid md:grid-cols-3 gap-8">
+                    {tours.map((tour) => {
+                        return (
+                            <Link className="hover:text-blue-500" key={tour.id} href={`/tours/${tour.id}`}>
+                                {/* creating of reponsive images */}
+                                <div className="relative h-48 mb=3">
+                                    <Image src={tour.image}
+                                        alt={tour.name}
+                                        priority
+                                        fill
+                                        className="object-cover rounded"
+                                        sizes="(max-width:760) 100vw, (max=width:1200) 50vw, 33vw"
+                                    />
+                                </div>
+
+                                <h2 >
+                                    {tour.name}
+                                </h2>
+                            </Link>
+                        )
+                    })}
+
+                </div>
             </section>
 
         </div>
