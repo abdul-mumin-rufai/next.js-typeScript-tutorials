f7e4218da495fff56f6d4c2de4fe67be000cc068
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-08 19:36:00 +0000

    saveUser: created a users.json file and save the list of users there

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,8 +1,32 @@
 'use server'
+import { isUtf8 } from "buffer";
+import { readFile, writeFile } from "fs/promises";
+
+interface User { 
+    firstName: string;
+    lastName: string;
+    id:string
+};
 export const createUser = async (formData: FormData) => {
+    // get the firstName from the input field
     const firstName = formData.get('firstName') as string;
+    // grt the lastName from the input field
     const lastName = formData.get('lastName') as string;
-    const rawData = Object.fromEntries(formData)
-    console.log({ firstName, lastName });
-    console.log(rawData);
+    const newUser: User = { firstName, lastName, id: Date.now().toString() }
+    await saveUser(newUser);
+};
+
+// we have to fetch the users and push the new user to it 
+export const fetchUsers = async (): Promise<User[]> => { 
+    const results = await readFile('users.json', { encoding: 'utf8' });
+    const users = results ? JSON.parse(results) : [];
+    return users;
+};
+
+
+// we have push the new user to the list of users and updated the users.json file
+const saveUser = async (user:User):Promise<void> => { 
+    const users = await fetchUsers();
+    users.push(user);
+    await writeFile('users.json', JSON.stringify(users));
 };
