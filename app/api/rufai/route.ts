868e33033dda26dbbcc349baf96d9210d82999da
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-05-15 10:35:22 +0000

    routeHandlerPOST: create a new user using the POST request in the routr handler

--- app/api/rufai/route.ts
+++ app/api/rufai/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fetchUsers, saveUser } from "@/utils/actions";
-import { URL } from "url";
 
 type ApiUser = {
     id: number;
@@ -15,5 +14,14 @@ export const GET = async (request: NextRequest) => {
 
     const users = await fetchUsers();
     //const mumin: ApiUser[] = [{ id: 15, name: 'Queit Developer' }, { id: 57, name: 'HENNGE Intern' }];
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.json(users)
+};
+
+export const POST = async (request:Request) => { 
+    const user = await request.json();
+    const newUser = { ...user, id: Date.now().toString() };
+    console.log(newUser);
+    
+    await saveUser(newUser);
+    return Response.json({msg: 'user added sucessfully'})
 };
